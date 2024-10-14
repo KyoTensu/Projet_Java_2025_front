1fe71dee1348f5d56a34aabca0152e1a8f447239
azekyoo <victor.gaspard.mail@gmail.com>
2024-10-14 21:52:32 +0900

    Qualité visuelle et interactivité des boutons améliorées

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core"
-
+import { Router } from "@angular/router"; 
 @Component({
   selector: "epf-home",
   templateUrl: "./home.component.html",
@@ -7,10 +7,14 @@ import { Component, OnInit } from "@angular/core"
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  goToBientot(): void {
+    this.router.navigate(['/bientot']); // Navigate to /bientot
   }
 
 }
