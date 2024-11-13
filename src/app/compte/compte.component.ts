63941154ee4e8c6e479105497add32625cc0fe76
KyoTensu <loloduvaucluse@gmail.com>
2024-11-13 16:48:58 +0100

    ÃMise en place intermédiaire de la page de connexion (erreur dans requête)

--- src/app/compte/compte.component.ts
+++ src/app/compte/compte.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from "../models/utilisateur.model"
+import { UtilisateurService } from "../services/utilisateur.service"
+import { Router } from "@angular/router"
 
 @Component({
-  selector: 'epf-compte',
-  templateUrl: './compte.component.html',
-  styleUrls: ['./compte.component.scss']
+  selector: "epf-compte",
+  templateUrl: "./compte.component.html",
+  styleUrls: ["./compte.component.scss"],
 })
 export class CompteComponent implements OnInit {
+  user: Utilisateur | null = null
+  mail: string = ''
+  password: string = ''
 
-  constructor() { }
+  constructor(private userService: UtilisateurService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  onConnectButtonClick() {
+    this.userService.connectUser(this.mail, this.password).subscribe(
+      (data: Utilisateur) => {
+        this.user = data;
+        localStorage.setItem('user_id', this.user.id.toString())
+        this.router.navigate(['/bientot'])
+      },
+      error => {
+        console.error('Error fetching the user:', error);
+      }
+    );
   }
-
 }
---
+++ src/app/services/utilisateur.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Observable } from "rxjs"
+import { Utilisateur } from "../models/utilisateur.model"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilisateurService {
+
+  constructor(private http: HttpClient) { }
+
+  private userURL = "http://localhost:8080/utilisateurs"
+
+  connectUser(mail: string, password: string): Observable<Utilisateur>{
+    return this.http.get<Utilisateur>(this.userURL+"/"+mail+"/"+password)
+  }
+}
